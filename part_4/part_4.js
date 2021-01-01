4b71a4875603e696c6666d1144c8b51678e0c6f1
tetra-shayeb <mohammednm40@gmail.com>
2021-01-01 23:19:25 +0200

    try to solve part_4

--- part_4/part_4.js
+++ part_4/part_4.js
@@ -12,8 +12,33 @@ return 'NO' if the parentheses in the input string are not balanced,.
 console.log('PART 4');
 
 const isABalancedString = () => {
-}
+  // WRITE YOUR CODE BELOW THIS LINE
+  let val = s.split("")
+  let is = "[{(";
+  let are = "]})";
+  let stack = [];
 
+  for (let i in val) {
+    if (is.includes(val[i] && !(i == (val.length - 1)))) {
+      stack.push(val[i]);
+      continue;
+    }
+
+    if (are.includes(val[i]) && are.indexOf(val[i]) == is.indexOf(stack.slice(-1)[0])) {
+      stack.pop();
+    } else {
+      return "NO";
+    }
+
+    if (i == (val.length - 1)) {
+      if (!stack.length) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    }
+  }
+};
 
 /*
 
