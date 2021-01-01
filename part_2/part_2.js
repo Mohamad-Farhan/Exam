b48d2a43319a2cf06165de7fc7af828d683e2e05
tetra-shayeb <mohammednm40@gmail.com>
2021-01-01 22:21:52 +0200

    solved part_2

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -9,27 +9,35 @@ Q1) What is the query for displaying all the databases?
 
 Q2) What is the query for using a specific database?
 
+('SELECT * FROM tabele_name ')
 
 Q3) What is the query for creating a new database called `meraki`?
 
+CREATE DATABASE meraki
 
 Q4) What is the query for deleting a database called `meraki`?
 
+DROP DATABASE meraki
 
 Q5) What is the query for displaying all the tables for a database called `meraki`?
 
+SELECT meraki FROM INFORMATION_SCEHEMA 
 
 Q6) What is the query for displaying the columns of a table called `users`?
 
+SHOW COMLIMS FROM users IN meraki
 
 Q7) What is the query to insert information into a table called `users` that has four columns (name, email, password, age)?
 
+INSERT INTO user (name, email, password, age)
 
 Q8) What is the query to update the `name` of a user that has `info@meraki-academy.org` as an email?
 
+UPDATE user SET name = info@meraki-academy WHERE condion;
 
 Q9) What is the query to delete a user that has `info@meraki-academy.org` as an email?
 
+DELETE FROM user WHERE email = info@meraki-academy.org
 
 Q10) What is the query to perform a left join on two tables (users, posts) depending on the user_id? (user_id is a FK in `posts` table)
 
